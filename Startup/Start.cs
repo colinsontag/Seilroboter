3eecaa12d07b727574a61f5520dc0469e5d1c470
Colin Sontag <Colin.Sontag@live.de>
2024-04-09 18:15:41 +0200

    ConfigTools Added

---
+++ Startup/Start.cs
@@ -0,0 +1,14 @@
+﻿using ConfigManger;
+using Microsoft.VisualBasic;
+
+namespace Workflow
+{
+    public class Startup
+    {
+        public static void Start()
+        {
+            RobotConfig InitialRobotConfig = ConfigTools.LoadFromXML(@"W:\Seilroboter\ConfigManger\RobotConfig.xml");
+
+        }
+    }
+}
