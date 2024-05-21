74e5102cf384c9daa7516ad4144a6bf6f8354e99
Colin Sontag <sontag@swms.de>
2024-05-21 15:11:55 +0200

    pyqt added

---
+++ PYQTtest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Python.Runtime;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", @"D:\Python\python312.dll");
+        // Initialize the Python runtime
+        PythonEngine.Initialize();
+        
+        try
+        {
+            using (Py.GIL())
+            {
+                // Import the Python script and call the main function
+                dynamic PyQt5 = Py.Import("PyQt5");
+                dynamic sys = Py.Import("sys");
+                sys.path.append("C:\\Users\\csontag\\Documents\\Studium\\Seilroboter");
+                
+
+                // Import the Python script and call the main function
+                dynamic ui = Py.Import("ui");
+                ui.main();
+                
+            }
+        }
+        catch (PythonException ex)
+        {
+            Console.WriteLine("Python error: " + ex.Message);
+            Console.WriteLine("Stack trace: " + ex.StackTrace);
+        }
+        finally
+        {
+            // Shutdown the Python runtime
+            PythonEngine.Shutdown();
+        }
+    }
+}
