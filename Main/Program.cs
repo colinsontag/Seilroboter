ffaf6146ff04b490e9fa2b28086b2503ae2f400f
Colin Sontag <sontag@swms.de>
2024-08-01 12:03:52 +0200

    stopwatsch added

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,28 +1,32 @@
 ﻿using Startup;
+using System.Diagnostics;
 
 namespace Main
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Seilroboter Anwendung Gestartet");
+            Stopwatch stopwatch = new Stopwatch();
             try
             {
+                stopwatch.Start();
                 StartupWorkflow.Start();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error: " + ex.Message);                
+                Console.WriteLine("Error: " + ex.Message);
             }
             finally
             {
+                stopwatch.Stop();
+
                 Console.WriteLine("Seilroboter Anwendung Beendet");
+                Console.WriteLine("Ausgeführte Zeit: " + stopwatch.Elapsed);
                 Console.WriteLine("Drücken Sie eine Taste zum Beenden...");
                 Console.ReadKey();
             }
-            
-            
         }
     }
 }
