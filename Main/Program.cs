0ebc84d18148a2b7b48f90dfc0917883f933fd86
Colin Sontag <sontag@swms.de>
2024-08-01 12:34:27 +0200

    final diasabling of motors

--- Main/Program.cs
+++ Main/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Startup;
+using System.Device.I2c;
+using System;
 using System.Diagnostics;
+using Machine;
 
 namespace Main
 {
@@ -21,6 +24,27 @@ namespace Main
             finally
             {
                 stopwatch.Stop();
+                const int busId = 1;
+                Console.WriteLine("motor stop routine");
+                // I2C-Adressen der Arduinos
+                const int arduinoAddress1 = 0x08;
+                const int arduinoAddress2 = 0x09;
+                const int arduinoAddress3 = 0x0A;
+                // Initialisiere I2C-Verbindung zu jedem Arduino
+                var i2cSettings1 = new I2cConnectionSettings(busId, arduinoAddress1);
+                var i2cDevice1 = I2cDevice.Create(i2cSettings1);
+
+                var i2cSettings2 = new I2cConnectionSettings(busId, arduinoAddress2);
+                var i2cDevice2 = I2cDevice.Create(i2cSettings2);
+
+                var i2cSettings3 = new I2cConnectionSettings(busId, arduinoAddress3);
+                var i2cDevice3 = I2cDevice.Create(i2cSettings3);
+                var motorOn = false;
+                var motorPlus = false;
+                Console.WriteLine("Start Disabling Motors");
+                DriveInteraction.SendMotor(i2cDevice1, motorOn, motorPlus);
+                DriveInteraction.SendMotor(i2cDevice2, motorOn, motorPlus);
+                DriveInteraction.SendMotor(i2cDevice3, motorOn, motorPlus);
 
                 Console.WriteLine("Seilroboter Anwendung Beendet");
                 Console.WriteLine("Ausgeführte Zeit: " + stopwatch.Elapsed);
